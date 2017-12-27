1584fd6d860980fbe7544ac3331f907bf715a54e
s.salamanca.lopez <s.salamanca.lopez@hotmail.com>
2017-12-27 10:33:44 -0500

    Custom pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,7 +11,8 @@ import {
     EventRouteActivator,
     EventListResolver,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    DurationPipe
 } from './events/index'
 import { EventsAppComponent } from './events-app.component'
 import { NavBarComponent } from './nav/navbar.component'
@@ -39,7 +40,8 @@ import { AuthService } from './users/auth.service';
         Error404Component,
         CreateSessionComponent,
         SessionListComponent,
-        CollapsibleWellComponent
+        CollapsibleWellComponent,
+        DurationPipe
     ],
     providers: [
         EventService,
