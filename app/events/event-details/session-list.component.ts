c52568ff6712f070484906adfc5ae33e4c13504f
s.salamanca.lopez <s.salamanca.lopez@hotmail.com>
2017-12-27 14:19:12 -0500

    Ordenamiento de datos

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -9,11 +9,13 @@ import { filter } from 'rxjs/operator/filter';
 export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[]
     @Input() filterBy: string
+    @Input() sortBy: string
     visibleSessions: ISession[] = []
 
     ngOnChanges() {
         if (this.sessions) {
             this.filterSessions(this.filterBy)
+            this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc)
         }
     }
 
@@ -25,4 +27,14 @@ export class SessionListComponent implements OnChanges {
             this.visibleSessions = this.sessions.filter(session => { return session.level.toLocaleLowerCase() === filter })
         }
     }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+    if (s1.name > s2.name) return 1
+    else if (s2.name === s1.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+    return s2.voters.length - s1.voters.length
 }
