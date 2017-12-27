1b7080dfff978eef49fdae5c737dbb9f819724eb
s.salamanca.lopez <s.salamanca.lopez@hotmail.com>
2017-12-27 10:20:22 -0500

    Filters

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,14 +7,14 @@ import { IEvent } from './index';
     template: `
     <div [routerLink]="['/events', event.id]" class='well hoverwell thumbnail'>
         <h2>{{event?.name}}</h2>
-        <div>Date: {{event?.date}}</div>
+        <div>Date: {{event?.date | date:'shortDate'}}</div>
         <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
             Time: {{event?.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
             <span *ngSwitchDefault>(Normal Start)</span>
         </div>
-        <div>Price: \${{event?.price}}</div>
+        <div>Price: {{event?.price | currency:'USD':true}}</div>
         <div *ngIf="event?.location">
             <span>Location {{event?.location?.address}}</span>            
             <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
